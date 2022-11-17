ebb5f9cdd641b63c5b958229e3042bc8e821dd0b
judy.cyh <judy.cyh@link8tech.tw>
2022-11-17 15:45:08 +0800

    add SignPage

--- src/pages/signPage/SignPage.jsx
+++ src/pages/signPage/SignPage.jsx
@@ -1,14 +1,27 @@
 import Navbar from '@components/Navbar/Navbar'
 import './signPage.less'
-import { Tabs, Tab } from '@mui/material'
+import React,{useState} from 'react'
+import { TabUnstyled,TabsListUnstyled,TabPanelUnstyled,TabsUnstyled } from '@mui/base'
+import SignFile from '@components/SignFile/SignFile'
+
 const SignPage = () =>{
+    const selected = document.querySelector('.tabSign')
+    
     return(
         <main>
             <Navbar text={'歷史文件'} link={"/history"}/>
-            <Tabs>
-                <Tab label="簽署文件" href="/sign#file"/>
-                <Tab label="建立簽名檔" href="/sign#signature"/>
-            </Tabs>
+            <TabsUnstyled className="tabsSection " defaultValue={0} selectionFollowsFocus>
+                <TabsListUnstyled className="tabListSection">
+                    <TabUnstyled className='tabSign tabs'>簽署文件</TabUnstyled>
+                    <TabUnstyled className='tabSignature tabs'>建立簽名檔</TabUnstyled>
+                </TabsListUnstyled>
+                <TabPanelUnstyled value={0}>
+                    <SignFile/>
+                </TabPanelUnstyled>
+                <TabPanelUnstyled value={1}>
+                    hiiii
+                </TabPanelUnstyled>
+            </TabsUnstyled>
         </main>
     )
 }
