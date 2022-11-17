0702a5a36c6f07d89d3930f6ff1847da53884a6a
judy.cyh <judy.cyh@link8tech.tw>
2022-11-17 18:12:19 +0800

    add uploadFile function

--- src/components/UploadFile/UploadFile.jsx
+++ src/components/UploadFile/UploadFile.jsx
@@ -1,7 +1,26 @@
 import { Button } from '@mui/material'
 import './UploadFile.less'
-
+import React, {useState} from 'react'
 const UploadFile = () =>{
+    const [imageSrc,setImageSrc] = useState('')
+    const [imageAlt, setImageAlt] = useState('')
+
+    const handleChange = (e)=>{
+        console.log(e.target.files)
+    }
+    const handleOnPreview = (event) => {
+        const file = event.target.files[0];
+        const reader = new FileReader();
+        reader.addEventListener("load", function () {
+          setImageAlt(file.name)
+          setImageSrc(reader.result)
+        }, false);
+    
+        if (file) {
+          reader.readAsDataURL(file);
+        }
+      };
+    
     return (
         <section className="uploadSection">
             <div className='descriptionSection'>
@@ -9,10 +28,11 @@ const UploadFile = () =>{
                     <li>檔案格式須為PDF或IMG</li>
                     <li>檔案大小上限為10MB</li>
                 </ul>
-                <div className="descriptionBtn"><label for="upload">上傳文件</label><input style={{'opacity':0}} type="file" id="upload" name="upload" accept=".jpg, .jpeg, .png, .pdf"/></div>
+                <div className="descriptionBtn"><label for="upload">上傳文件</label><input onChange={handleOnPreview} style={{'opacity':0}} type="file" id="upload" name="upload" accept=".jpg, .jpeg, .png, .pdf"/></div>
             </div>
             <div className='fileSection'>
-                文件顯示區
+                {!imageSrc && (<p>文件顯示區</p>)}
+                <img src={imageSrc} alt={imageAlt}/>
             </div>
         </section>
     )
