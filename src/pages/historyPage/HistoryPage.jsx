73917610fdb64d7ac12c1c37f6908920076522c6
judy.cyh <judy.cyh@link8tech.tw>
2022-11-18 16:13:51 +0800

    add rwd breakpoint

--- src/pages/historyPage/HistoryPage.jsx
+++ src/pages/historyPage/HistoryPage.jsx
@@ -1,12 +1,16 @@
+import React,{useState} from 'react'
 import Navbar from '@components/Navbar/Navbar'
 import './historyPage.less'
 import Photo from '@assets/photo.jpg'
 import { Button } from '@mui/material'
 const HistoryPage = () =>{
+    const [history,setHistory] = useState([Photo,Photo])
     return(
         <main>
             <Navbar text={'回到首頁'} link={"/"}/>
-            <div className="fileSection">
+            <div className='historySection'>
+            {history.map((photo)=>(
+                <div className="fileSection">
                 <h3 className="fileTitle">檔案名稱</h3>
                 <div className="filePhoto">
                     <img src={Photo} alt="title"/>
@@ -15,6 +19,8 @@ const HistoryPage = () =>{
                     <Button variant='outlined' className="delete">刪除</Button>
                     <Button variant='contained' className="download">下載</Button>
                 </div>
+            </div>
+            ))}
             </div>
         </main>
     )
