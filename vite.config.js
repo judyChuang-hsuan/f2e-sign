8bf34b4fc94ae29ca1bf4298b094b5b8b22f1002
judy.cyh <judy.cyh@link8tech.tw>
2022-11-21 18:04:14 +0800

    add "utils" alias

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
   resolve:{
     alias:{
       '@components':path.resolve(__dirname,'./src/components'),
-      '@assets':path.resolve(__dirname,'./src/assets')
+      '@assets':path.resolve(__dirname,'./src/assets'),
+      '@utils':path.resolve(__dirname,'./src/utils')
     }
   }
 })
